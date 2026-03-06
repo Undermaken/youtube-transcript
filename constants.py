6f666bec1ffd85ae865bce0bc5cd7536896248e8
Matteo Boschi <mbmaken@gmail.com>
2026-03-06 14:59:43 +0100

    Bump version to v0.1.4

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 # to update the version, please run the release.py script
-APP_VERSION = "0.1.3"
+APP_VERSION = "0.1.4"
 APP_UPDATE = "06-03-2026"
 AUTHOR_NAME = "Matteo Boschi"
 PROJECT_LINK = "https://github.com/Undermaken/youtube-transcript"
